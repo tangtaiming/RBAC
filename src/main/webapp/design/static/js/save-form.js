f044ce4e349cd4b98ed8aa0707d9aae55bd9ba8d
tangtaiming <accountnum@sina.cn>
2018-08-29 09:26:03 +0800

    fix(loation):优化 切换菜单类型js添加
    无
    
    author:tangtaiming

--- src/main/webapp/design/static/js/save-form.js
+++ src/main/webapp/design/static/js/save-form.js
@@ -26,13 +26,20 @@ $(document).ready(function () {
      */
     $('input[name="saveMenuReVo.type"]').click(function() {
         var type = $(this).val();
+        $('#name-group').show();
+        $('#orderNum-group').show();
         if (type == "0") {
-            //隐藏 菜单URL 标识
+            $('#icon-group').show();
             $('#url-group').hide();
             $('#perms-group').hide();
         } else if (type == '1') {
+            $('#icon-group').show();
             $('#url-group').show();
             $('#perms-group').show();
+        } else {
+            $('#icon-group').hide();
+            $('#url-group').show();
+            $('#perms-group').hide();
         }
     });
 });
@@ -40,49 +47,58 @@ $(document).ready(function () {
 
 function saveMenu() {
     $("#save-menu").click(function() {
+        var typeZero = "0";
+        var typeOne = "1";
+        var typeTwo = "2";
         var params = {};
         //类型
         var menuType = $('input[name="saveMenuReVo.type"]');
-        menuType.each(function() {
+        var typeValue, typeKey;
+            menuType.each(function() {
             var menuChecked = this.checked;
             if (menuChecked) {
-                var typeKey = $(this).attr("name");
-                var typeValue = $(this).val();
+                typeKey = $(this).attr("name");
+                typeValue = $(this).val();
                 putParams(params, typeKey, typeValue);
             }
         });
-        //菜单名称
         putInput(params, "saveMenuReVo.menuId");
         //菜单名称
         putInput(params, "saveMenuReVo.name");
         //上级菜单
         putInputById(params, "parentId");
         putInputById(params, "parentName");
-        //菜单URL
-        putInput(params, "saveMenuReVo.url");
-        //授权
-        putInput(params, "saveMenuReVo.perms");
+        if (typeOne == typeValue || typeTwo == typeValue) {
+            //菜单URL
+            putInput(params, "saveMenuReVo.url");
+        }
+        if (typeOne == typeValue) {
+            //授权
+            putInput(params, "saveMenuReVo.perms");
+        }
         //排序
         putInput(params, "saveMenuReVo.orderNum");
-        //图标
-        putInput(params, "saveMenuReVo.icon");
+        if (typeZero == typeValue || typeOne == typeValue) {
+            //图标
+            putInput(params, "saveMenuReVo.icon");
+        }
         console.log(params);
-        $.ajax({
-            url : "/admin/saveMenu",
-            data : params,
-            type : 'post',
-            success : function ( result ) {
-                console.log("Show response success" + result);
-                if (result && result == "success") {
-                    window.location.href = "/admin/menuManagement";
-                } else {
-                    alert(result.trim());
-                }
-            },
-            error : function () {
-                alert('Save menu error');
-            }
-        });
+        // $.ajax({
+        //     url : "/admin/saveMenu",
+        //     data : params,
+        //     type : 'post',
+        //     success : function ( result ) {
+        //         console.log("Show response success" + result);
+        //         if (result && result == "success") {
+        //             window.location.href = "/admin/menuManagement";
+        //         } else {
+        //             alert(result.trim());
+        //         }
+        //     },
+        //     error : function () {
+        //         alert('Save menu error');
+        //     }
+        // });
     });
 }
 
